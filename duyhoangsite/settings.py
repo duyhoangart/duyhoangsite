0d21f810512f7062a99e3aa19260fdfc5de8e7c1
duyhoang <phungthithanhhuyen5054@gmail.com>
2025-12-04 09:12:11 +0700

    Fix SECRET_KEY

--- duyhoangsite/settings.py
+++ duyhoangsite/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-change-this-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
